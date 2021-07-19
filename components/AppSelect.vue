8e891877804194e17a99213594cdc50cba9fa178
Маруся <marusa@MacBook-Pro-Maria.local>
2021-07-19 15:13:17 +0400

    add component select

---
+++ components/AppSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label for="select">What {{ select }} do you want?</label>
+
+    <select
+      id="select"
+      @change="$emit('change', $event.target.value)"
+      :name="select"
+    >
+      <option value>-- Please select a {{ select }}</option>
+      <option v-for="option in selectOptions" :key="option.id" :value="option">
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    select: {
+      type: String,
+      default: "cuisine"
+    },
+    selectOptions: {
+      type: Array,
+      default: () => ["tacos", "pizza", "dim sumπ"]
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,28 @@
 <template>
-  <main>
-    <h1>Restaurants</h1>
+  <main class="container restaurant">
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+
+      <AppSelect @change="selectedReaustarant = $event" />
+      <pre>{{ $data }}</pre>
+    </div>
     <AppRestaurantInfo />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
 
 export default {
   components: {
-    AppRestaurantInfo
+    AppRestaurantInfo,
+    AppSelect
+  },
+  data() {
+    return {
+      selectedReaustarant: ""
+    };
   }
 };
 </script>
