95185867a11fcd5b1e9311c3aa3e10741d0e78a6
Маруся <marusa@MacBook-Pro-Maria.local>
2021-07-19 14:00:52 +0400

    add api

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ["~/assets/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~/plugins/getfood.server.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- store/index.js
+++ store/index.js
@@ -158,14 +158,35 @@ export const state = () => ({
 //   }
 // }
 
-// export const mutations = {
-//   updateValue: (state, payload) => {
-//     state.value = payload
-//   }
-// }
+export const mutations = {
+  updateFoodData: (state, data) => {
+    state.fooddata = data;
+  }
+};
 
-// export const actions = {
-//   updateActionValue({ commit }) {
-//     commit('updateValue', payload)
-//   }
-// }
+export const actions = {
+  async getFoodData({ state, commit }) {
+    if (state.fooddata.length) {
+      return;
+    }
+    try {
+      await fetch(
+        "https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": process.env.AWS_API_KEY
+          }
+        }
+      )
+        .then(response => response.json())
+        // eslint-disable-next-line arrow-parens
+        .then(data => {
+          commit("updateFoodData", data);
+        });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
+  }
+};
